d860c9ab53fb83e0c5dcc04b4fc49ed69b9ec8c0
Michael Bareford <Michael Bareford>
2018-08-06 10:46:30 +0100

    Added support for ARCHER and Cirrus platforms.

--- geomseries/python/gen_all_counter_plots.sh
+++ geomseries/python/gen_all_counter_plots.sh
@@ -1,6 +1,15 @@
-./gen_counter_plots.sh papi cray O0n24
-./gen_counter_plots.sh papi cray O3n24
-./gen_counter_plots.sh papi intel O0n24
-./gen_counter_plots.sh papi intel O3n24
-./gen_counter_plots.sh papi gnu O0n24
-./gen_counter_plots.sh papi gnu O3n24
+#!/bin/bash --login
+
+if [ "$1" == "archer" ] || [ "$1" == "" ]; then
+  ./gen_counter_plots.sh archer papi cray O0n24
+  ./gen_counter_plots.sh archer papi cray O3n24
+  ./gen_counter_plots.sh archer papi intel O0n24
+  ./gen_counter_plots.sh archer papi intel O3n24
+  ./gen_counter_plots.sh archer papi gnu O0n24
+  ./gen_counter_plots.sh archer papi gnu O3n24
+elif [ "$1" == "cirrus" ]; then
+  ./gen_counter_plots.sh cirrus papi intel O0n36
+  ./gen_counter_plots.sh cirrus papi intel O3n36
+  ./gen_counter_plots.sh cirrus papi gnu O0n36
+  ./gen_counter_plots.sh cirrus papi gnu O3n36
+fi
--- geomseries/python/gen_counter_plots.sh
+++ geomseries/python/gen_counter_plots.sh
@@ -1,9 +1,22 @@
-python analyse_counter_variations.py -data ../arc/$2/$3/$1_test.out -flops PAPI_FP_OPS
+#!/bin/bash --login
 
-python analyse_counter_variations.py -data ../arc/$2/$3/$1_test.out -cntr PERF_COUNT_HW_CACHE_L1D -flops PAPI_FP_OPS
-python analyse_counter_variations.py -data ../arc/$2/$3/$1_test.out -cntr PAPI_L2_DCA -flops PAPI_FP_OPS
-python analyse_counter_variations.py -data ../arc/$2/$3/$1_test.out -cntr PERF_COUNT_HW_CACHE_LL:ACCESS -flops PAPI_FP_OPS
-python analyse_counter_variations.py -data ../arc/$2/$3/$1_test.out -cntr PERF_COUNT_HW_CACHE_NODE:ACCESS -flops PAPI_FP_OPS
+if [ "$1" == "archer" ]; then
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -flops PAPI_FP_OPS -ymax 5.0
 
-mv *.eps ../arc/$2/$3/
-mv *.txt ../arc/$2/$3/
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PERF_COUNT_HW_CACHE_L1D -flops PAPI_FP_OPS
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PAPI_L2_DCA -flops PAPI_FP_OPS
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PERF_COUNT_HW_CACHE_LL:ACCESS -flops PAPI_FP_OPS
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PERF_COUNT_HW_CACHE_NODE:ACCESS -flops PAPI_FP_OPS
+elif [ "$1" == "cirrus" ]; then
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -single-flops PAPI_SP_OPS -double-flops PAPI_DP_OPS -ymax 6.0
+
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PERF_COUNT_HW_CACHE_L1D:ACCESS -single-flops PAPI_SP_OPS -double-flops PAPI_DP_OPS
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PAPI_L2_DCA -single-flops PAPI_SP_OPS -double-flops PAPI_DP_OPS
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PERF_COUNT_HW_CACHE_LL:ACCESS -single-flops PAPI_SP_OPS -double-flops PAPI_DP_OPS
+  python analyse_counter_variations.py -data ../arc/$3/$4/$2_test.out -cntr PERF_COUNT_HW_CACHE_NODE:ACCESS -single-flops PAPI_SP_OPS -double-flops PAPI_DP_OPS
+fi
+
+if ls ./*.eps 1> /dev/null 2>&1; then
+  mv *.eps ../arc/$3/$4/
+  mv *.txt ../arc/$3/$4/
+fi
